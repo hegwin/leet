96418fd2919b815d5bec3917b401d4aed8e73f5e
hegwin <zwt315@163.com>
2018-01-10 21:28:35 +0800

    [9] 2nd version, accepted, beats 24.75

--- 009-palindrome-number.rb
+++ 009-palindrome-number.rb
@@ -3,13 +3,5 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-  return false if x < 0
-
-  arr = x.to_s.split('')
-
-  while x.size > 1
-    return false if arr.shift != arr.pop
-  end
-
-  return true
+  x == x.to_s.reverse.to_i
 end
